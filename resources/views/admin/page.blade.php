25bbf4476e5e18ee1a742eb283a22d850890c025
vagrant <vagrant@extranet2.puphpet>
2017-11-13 15:59:34 +0000

    vue js now handles adding of rows to the page builder screen.

--- resources/views/admin/page.blade.php
+++ resources/views/admin/page.blade.php
@@ -5,17 +5,17 @@
 .grid-item { width: 25%; }
 .grid-item--width2 { width: 50%; }
 </style>
-<div class="container">
+<div id="page-template" class="container">
 
-	<div class="draggabilly-container">
+	<div  class="draggabilly-container">
 	@foreach($page->elements->where('type',1)->sortBy('position') AS $row)
-		<div class="row" 
+		<div id="{{ $row->id }}" class="row" 
 			style="height:{{$row->y_size}}px; @if($row->metaData)  background-color:{{ $row->metaData->background_color }}; color:{{ $row->metaData->color }}; @endif ">
 			@if($row->metaData)
 				{{ $row->metaData->content }}
 			@endif			
 			@foreach($page->elements->where('type', 2)->where('parent_id', $row->id)->sortBy('position') AS $col)
-				<div class="col-md-{{ $col->x_size }} draggable"  @if($col->metaData) style="background-color:{{ $col->metaData->background_color }};color:{{ $col->metaData->color }};height:{{$col->y_size}}%;border:{{ $col->metaData->border }}" @endif>
+				<div id="{{ $col->id }}" class="col-md-{{ $col->x_size }}"  @if($col->metaData) style="background-color:{{ $col->metaData->background_color }};color:{{ $col->metaData->color }};height:{{$col->y_size}}%;border:{{ $col->metaData->border }}" @endif>
 					@if($col->metaData)
 						{{ $col->metaData->content }}
 					@endif
@@ -25,19 +25,27 @@
 		</div>
 	@endforeach
 	</div>
+
+	<template-row v-for="row in row_count"  :rownum="rows.state.count"></template-row>
+
+	<div class="row">
+		<div class="col-md-2 col-xs-offset-5">
+			<button v-on:click="addRow()" type="button" class="btn btn-default add_row_button">Add Row</button>
+		</div>
+	</div>
 	
 </div>
 
-
-
-
+@foreach($page->elements AS $element)
+	<input class="page-element hidden" data-element-id="{{ $element->id }}"  value=""/>
+@endforeach
 
 @push('scripts-admin')
 <script type="text/javascript">
 	$(document).ready(function(){
 
 	});
-	//function listener(/* parameters */) {
+	//function listener() {
 	//  draggie = $(this).data('draggabilly');
 	//  console.log($(this).parent());
 	//  console.log( 'eventName happened', draggie.position.x, draggie.position.y );
@@ -46,9 +54,38 @@
 	//	containment: '.draggabilly-container',
 	//	grid: [ 50, 50 ]
 	//})
-    //
 	//$draggable.on( 'dragEnd', listener );
 
+	const rows = new Vuex.Store({
+	  state: {
+	    count: 0
+	  },
+	  mutations: {
+	    increment (state) {
+	      state.count++
+	    }
+	  }
+	})
+
+	var page = new Vue({
+	  el: '#page-template',
+	  methods: {
+	  	addRow: function() {
+		rows.commit('increment')
+	    }
+	  }
+	})
+
+
+	Vue.component('template-row', {
+	  props: ['rownum'],
+	  data: function () {
+	    	return {
+			counter: this.rownum
+	    	}
+	  },
+	  template: '<div :data-element-id="counter"  data-element-type="2" class="row new-element">ROW</div></div>'
+	})
 
 </script>
 @endpush
--- resources/views/layouts/admin/layout.blade.php
+++ resources/views/layouts/admin/layout.blade.php
@@ -10,9 +10,10 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
+    	<!-- Styles -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">   
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/grapesjs/0.12.17/css/grapes.min.css" rel="stylesheet">
 </head>
 <body>		
 	<div class="container-fluid">
@@ -87,7 +88,10 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+	<script src="https://unpkg.com/vuex"></script>
 	<script src="https://unpkg.com/draggabilly@2/dist/draggabilly.pkgd.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/grapesjs/0.12.17/grapes.min.js"></script>
+	
 
 
 
